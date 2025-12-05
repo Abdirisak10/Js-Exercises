ee9633769bc82b32b8a3a7e4a6f0857930f03ec2
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-04 19:27:17 -0800

    my first Exercise of Javascript

---
+++ exercise 1/script.js
@@ -0,0 +1,27 @@
+// The Arthematic Operations Exercise , the first Exercise
+
+let x=7;
+let y=3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+// The modulus 
+console.log(x % y);
+
+// Increment
+// 1 - pre-increment
+console.log(++y);
+// 2- post increment
+console.log(y++);
+console.log(y);
+
+
+// Decrement
+// 1- pre decrement
+console.log(--x);
+// Post Decrement
+console.log(x--);
+console.log(x);
