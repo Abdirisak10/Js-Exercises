9e0cf581ea0e16178b2ff1329896b11cccbe39f8
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-05 19:47:48 -0800

    waxaan kusoo diray Exercise 3

---
+++ Exercise 3/script.js
@@ -0,0 +1,15 @@
+// Logical  Operators
+
+let isSunny = true;
+let isWeekend = true;
+
+let result = "You can go to the beach"
+
+console.log(isSunny && isWeekend + " " + result);
+
+
+// Not part of the Exercise it only practice
+
+console.log(isSunny || isWeekend);
+
+console.log(!isSunny);
