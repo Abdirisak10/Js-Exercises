1a91f7f857e9974f448ee33ec58fb2df937031a5
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-06 09:41:20 -0800

    Exercise 4 in js

---
+++ Exercise 4/script.js
@@ -0,0 +1,20 @@
+// Function declaration
+
+function add(a,b){
+    console.log(a + b);
+    // return a+b;
+}
+add(10,40);
+add(100,200);
+add(30,60);
+// let natiijo = add(50,40);
+// console.log(natiijo / 2);
+
+
+// Function Expression
+const addition = function(a,b){
+    console.log(a+b);
+}
+add(10,5);
+add(1000,1000);
+add(1,5);
