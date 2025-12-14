e8fc048d30a0143f14ec765c5e40d0d8822475ac
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-13 18:32:21 -0800

    Adding Exercise 12 and 13

---
+++ Exercise 12/script.js
@@ -0,0 +1,6 @@
+
+const Fruits=["apple","Banana","cherry"];
+for(let fruit = 0; fruit<Fruits.length; fruit++){
+    console.log(Fruits[fruit]);
+}
+
---
+++ Exercise 13/main.js
@@ -0,0 +1,8 @@
+
+
+let i=5;
+
+while(i>0){
+    console.log("the reverse result is: "+ i)
+    i--;
+}
