be92ae82ac4c1420bb198547aa42da9476ea1f30
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-19 09:33:39 -0800

    adding exercise 14 and 15

---
+++ Exercise 14/script.js
@@ -0,0 +1,6 @@
+let Enter;
+do{
+    Enter=prompt("Enter a Number that is greater than 10:");
+}
+while(Enter <=9);
+console.log("Your are Now the right one");
---
+++ Exercise 15/main.js
@@ -0,0 +1,14 @@
+const people=[
+    {name:"abdi", age:"26", city:"Middle-shabelle"},
+    {name:"Bob", age:"30", city:"mogadishu"},
+    {name:"charlie", age:"40", city:"Factory"}
+]
+console.log("properties and values of each One")
+for(let peoples in people){
+    console.log("-----")
+
+  for(let dadka of people){
+    console.log(dadka.people);
+  }
+}
+
