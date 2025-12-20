2511f969451c47c1f7176e6ee355af907fbbec0c
Abdirisak10 <abdirrisaq.ali@gmail.com>
2025-12-20 10:03:11 -0800

    i added exerices 16,17 and 18

---
+++ Exercise 17/app.js
@@ -0,0 +1,10 @@
+let temp=-1;
+if(temp<0){
+    console.log("It is very gold day")
+}else if(temp<15){
+    console.log("it is gold day")
+}else if(temp<25){
+    console.log("It is warm day")
+}else{
+    console.log("iIt is Hot day")
+}
---
+++ Exercise 18/app.js
@@ -0,0 +1,3 @@
+let grade=60;
+let fariinta = grade>=60? "you passed": "you failed";
+console.log(fariinta);
